e688efe6c5ad09a1512f473a15f5e1dc8731fda2
ycen <yin.cen@tworoadssoftware.com>
2015-12-01 22:19:37 -0500

    initial commit

---
+++ TimeSpeak/NYJson.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TimeSpeak
+{
+    
+public class Rootobject
+{
+public string status { get; set; }
+public string copyright { get; set; }
+public string section { get; set; }
+public DateTime last_updated { get; set; }
+public int num_results { get; set; }
+public List<Result> results { get; set; }
+}
+
+public class Result
+{
+    public string section { get; set; }
+    public string subsection { get; set; }
+    public string title { get; set; }
+    public string abstra { get; set; }
+    public string url { get; set; }
+    public string byline { get; set; }
+    public string item_type { get; set; }
+    public DateTime updated_date { get; set; }
+    public DateTime created_date { get; set; }
+    public DateTime published_date { get; set; }
+    public string material_type_facet { get; set; }
+    public string kicker { get; set; }
+    public string[] des_facet { get; set; }
+    public object org_facet { get; set; }
+    public object per_facet { get; set; }
+    public object geo_facet { get; set; }
+    public object multimedia { get; set; }
+}
+
+}
---
+++ TimeSpeak/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Speech.Synthesis;
+
+using Newtonsoft.Json;
+
+namespace TimeSpeak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sURL;
+            sURL = "http://api.nytimes.com/svc/topstories/v1/world.json?api-key=e40918abbccbb231b9558826929ef1a6:6:69444533";
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(sURL);
+            wrGETURL.Proxy = WebProxy.GetDefaultProxy();
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+            StreamReader objReader = new StreamReader(objStream);
+            string line = "";
+            line = objReader.ReadLine();
+            line = line.Replace("\"abstract\"","\"abstra\"");
+            //Console.WriteLine(line);
+            dynamic stories = JsonConvert.DeserializeObject<Rootobject>(line);
+            //int nlist = stories.results.length;
+            //string abs = stories.results[0].abstra;
+            //Console.WriteLine(abs);
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+
+                // Configure the audio output. 
+                synth.SetOutputToDefaultAudioDevice();
+                for (int i = 0; i < stories.num_results; i++)
+                {
+                    // Speak a string synchronously.
+                    string number = i.ToString();
+                    synth.Speak(number+ "\n" + stories.results[i].title);
+                    synth.Speak(stories.results[i].abstra);
+                }
+            }
+        }
+    }
+}
